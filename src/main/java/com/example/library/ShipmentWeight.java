72dd826a6508e9afd6193202257e7b1ee3b9905f
aditi gopal <gopaladiti@gmail.com>
2020-06-29 16:04:58 -0400

    Refactored code

--- src/main/java/com/example/library/ShipmentWeight.java
+++ src/main/java/com/example/library/ShipmentWeight.java
@@ -1,63 +1,126 @@
 package com.example.library;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 public class ShipmentWeight {
 
     private List<Integer> weights = new ArrayList<>();
     private Integer noOfDays = 0;
-    Integer minAnswer;
-    Integer answer;
-    int iteration;
+    Integer leastWeight;
+    Integer sumOfElements;
+    List<Integer> sumOfSums = new ArrayList<>();
     int noOfPackages;
     List<Data> daysArray = new ArrayList<>();
+    Scanner scanner = new Scanner(System.in);
 
     class Data {
         Integer index;
         Integer count;
+        Data(Integer index, Integer count) {
+            this.index = index;
+            this.count = count;
+        }
     }
 
+    /*
+     * Gets inputs from user and validates
+     */
     private void getInputs() {
-        Scanner scanner = new Scanner(System.in);
         System.out.println("Enter the array of weights, for example: 1,2,3,4,5 \n");
-        String value = scanner.next();
+        String value = validateArray();
+
         if (value != null) {
             String[] strArr = value.split(",");
             for (String s : strArr) {
                 weights.add(Integer.parseInt(s.trim()));
             }
         }
-        System.out.println("Enter the no of days: \n");
-        noOfDays = scanner.nextInt();
         noOfPackages = weights.size();
+
+        System.out.println("Enter the no of days: \n");
+        validateNoOfDays();
     }
 
+    /*
+     * Validates the input array
+     */
+    public String validateArray() {
+        String value = scanner.nextLine();
+        boolean matched = false;
+        while (!matched) {
+            Pattern inputArrayPattern = Pattern.compile(String.format("^(\\d+(\\S)+$(,\\d+)*)$"));
+            matched = inputArrayPattern.matcher(value).matches();
+            if(!matched) {
+                System.out.println("Invalid array format!");
+                System.out.println("Enter the array of weights, for example: 1,2,3,4,5 \n");
+                value = scanner.nextLine();
+            }
+        }
+        return value;
+    }
+
+    /*
+     * Validates the input number of days
+     */
+    public void validateNoOfDays() {
+        String days = scanner.nextLine();
+        boolean matched = false;
+        while (!matched) {
+            Pattern inputArrayPattern = Pattern.compile(String.format("^(\\d+)$"));
+            matched = inputArrayPattern.matcher(days).matches();
+            if (!matched) {
+                System.out.println("Invalid number of days!");
+                System.out.println("Enter the no of days: \n");
+                days = scanner.nextLine();
+            } else {
+                noOfDays = Integer.parseInt(days);
+                if (noOfDays > noOfPackages || noOfDays == 0) {
+                    matched = false;
+                    System.out.println("The number of days should be less than the number of weights in the array, but not 0!");
+                    System.out.println("Enter the no of days: \n");
+                    days = scanner.nextLine();
+                }
+            }
+        }
+    }
+
+    /*
+     * Prints the user inputs to the console
+     */
     public void printInputs() {
         System.out.println("Input Array:");
-        System.out.print("[");
+        System.out.print("[ ");
         for (Integer wt : weights) {
             System.out.print(wt + " ");
         }
         System.out.println("]");
-        System.out.println("No of days: \n" + noOfDays);
+        System.out.println("No of days: \n" + noOfDays + "\n");
     }
 
+    /*
+     * Populates 'daysArray' with an array of 'Data' elements.
+     * For each day, there will be a 'Data' element containing
+     * 'index' as the start index of the subset to be created
+     * and 'count' as the number of elements, from the start index
+     * to be included in the subset
+     */
     public void prepareDaysArray() {
         int noOfElements = weights.size() - (noOfDays - 1);
-        Data dataValue = new Data();
-        dataValue.index = 0;
-        dataValue.count = noOfElements;
+        Data dataValue = new Data(0,noOfElements);
         daysArray.add(dataValue);
         for(int i=1; i<noOfDays; i++) {
-            dataValue = new Data();
-            dataValue.index = daysArray.get(i-1).index + daysArray.get(i-1).count;
-            dataValue.count = 1;
+            int index = daysArray.get(i-1).index + daysArray.get(i-1).count;
+            dataValue = new Data(index, 1);
             daysArray.add(dataValue);
         }
     }
 
+    /*
+     * Returns the subsets of 'weights' list based on the index and count
+     * for each Day mentioned in the daysArrForPackages list
+     */
     public List<List<Integer>> getListOfPackages(List<Data> daysArrForPackages) {
         List<List<Integer>> listOfPackageCombinations = new ArrayList<>();
         for(int i=0; i<noOfDays; i++) {
@@ -72,6 +135,10 @@ public class ShipmentWeight {
         return listOfPackageCombinations;
     }
 
+    /*
+     * Returns the maximum sum of the sum of subset elements of the
+     * particular combination
+     */
     public Integer getMaxSum(List<Data> daysArrForSum) {
         List<List<Integer>> combList = getListOfPackages(daysArrForSum);
         int maxSum = 0;
@@ -83,10 +150,13 @@ public class ShipmentWeight {
         return maxSum;
     }
 
+    /*
+     * Prints the subsets of weights for each day for the particular
+     * combination
+     */
     public void printDaysArr(List<Data> daysArrForPrint) {
         for(int i=0; i<noOfDays; i++) {
-            System.out.print("Day: " + i + " index: " + daysArrForPrint.get(i).index
-            + " count: " + daysArrForPrint.get(i).count + " ");
+            System.out.print("Day: " + (i+1) + " ");
             System.out.print("[");
             for(int j=daysArrForPrint.get(i).index;
             j<daysArrForPrint.get(i).index + daysArrForPrint.get(i).count; j++) {
@@ -96,51 +166,45 @@ public class ShipmentWeight {
         }
     }
 
+    /*
+     * Finds all combinations of subsets of weights based on index (.i.e. start index)
+     * and count (.i.e. number of elements from start index) in List<Data> daysArrForComb
+     */
     public void findAllCombinations(List<Data> daysArrForComb, int n) {
         List<Data> daysArrLocal = new ArrayList<>();
-        for (Data d : daysArrForComb) {
-            daysArrLocal.add(d);
-        }
+        daysArrLocal = daysArrForComb.stream().map(data ->
+                new Data(new Integer(data.index.intValue()), new Integer(data.count.intValue())))
+                .collect(Collectors.toList());
 
         if (n == (noOfDays - 1))
             return;
-
-        System.out.println("******************Before**************");
-        for (int i = 0; i < noOfDays; i++) {
-            System.out.println("Day: " + i + " index: " + daysArrLocal.get(i).index
-                    + " count: " + daysArrLocal.get(i).count + " ");
-        }
-
-        System.out.println("******************Iteration**************");
 
         while (daysArrLocal.get(n).count > 1) {
             daysArrLocal.get(n).count--;
             daysArrLocal.get(n + 1).index--;
             daysArrLocal.get(n + 1).count++;
 
+            System.out.println("*************************************************");
+
             printDaysArr(daysArrLocal);
 
-            answer = getMaxSum(daysArrLocal);
-            getMinValue(answer);
+            sumOfElements = getMaxSum(daysArrLocal);
+            System.out.println("Max Sum for this combination: " + sumOfElements);
+            sumOfSums.add(sumOfElements);
+            getMinValue();
 
             findAllCombinations(daysArrLocal, n + 1);
-
-            System.out.println("After finding combinations...");
-            printDaysArr(daysArrLocal);
-
         }
-
-        System.out.println("***************After Iteration***************");
         n = n+1;
     }
 
-    public Integer getMinValue(Integer value) {
-        if(minAnswer == null) {
-            minAnswer = value;
-        } else if(value < minAnswer) {
-            minAnswer = value;
-        }
-        return minAnswer;
+    /*
+     * Get the minimum from sumOfSums which contains the maximum sums of subsets
+     * of each combination
+     */
+    public Integer getMinValue() {
+        leastWeight =  sumOfSums.stream().mapToInt(Integer::intValue).min().getAsInt();
+        return leastWeight;
     }
 
     public static void main(String[] args) {
@@ -151,15 +215,16 @@ public class ShipmentWeight {
         shipmentWeight.prepareDaysArray();
         shipmentWeight.printDaysArr(shipmentWeight.daysArray);
 
-        int maxSum = shipmentWeight.getMaxSum(shipmentWeight.daysArray);
-        System.out.println("Max Sum for a combination : " + maxSum);
+        Integer maxSum = shipmentWeight.getMaxSum(shipmentWeight.daysArray);
+        shipmentWeight.sumOfSums.add(maxSum);
+        System.out.println("Max Sum for this combination : " + maxSum);
 
-        shipmentWeight.getMinValue(maxSum);
+        shipmentWeight.getMinValue();
 
         for(int i=0; i<shipmentWeight.noOfDays; i++)
             shipmentWeight.findAllCombinations(shipmentWeight.daysArray, i);
 
-        System.out.println("Solution is: " + shipmentWeight.minAnswer);
+        System.out.println("\nLeast Weight Capacity is: " + shipmentWeight.leastWeight);
     }
 
 }
